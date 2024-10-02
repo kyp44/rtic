de4b02b7b9d66d2ea1e3d0e8cc1ffa788431452d
Dan Whitman <dwhitman44@mgail.com>
2024-10-02 18:26:46 -0400

    Implements the ATSAMD blinky example just using SysTick for now until the RTC driver is implemented.

--- examples/atsamd51j_blinky/src/main.rs
+++ examples/atsamd51j_blinky/src/main.rs
@@ -3,62 +3,65 @@
 #![no_main]
 #![no_std]
 
-use rtic_monotonics::nrf::rtc::prelude::*;
-atsamd_rtc_monotonic!(Mono);
+// TODO: For now use SysTick to make sure this works, until the RTC driver is complete
+//use rtic_monotonics::atsamd::rtc::prelude::*;
+use rtic_monotonics::systick::prelude::*;
 
-#[rtic::app(device = hal::pac, dispatchers = [TC0])]
+systick_monotonic!(Mono, 1000);
+
+#[rtic::app(device = pygamer::pac, dispatchers = [TC0])]
 mod app {
     use super::*;
-
-    use hal::gpio::{Level, Output, Pin, PushPull};
-    use hal::prelude::*;
+    use pygamer::{
+        hal::{clock::GenericClockController, prelude::*},
+        Pins, RedLed,
+    };
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        led: Pin<Output<PushPull>>,
+        red_led: RedLed,
     }
 
     #[init]
-    fn init(cx: init::Context) -> (Shared, Local) {
-        // Configure low frequency clock
-        hal::clocks::Clocks::new(cx.device.CLOCK).start_lfclk();
+    fn init(mut cx: init::Context) -> (Shared, Local) {
+        // Set up the clocks
+        // TODO: May not need this once the RTC driver is used.
+        let _ = GenericClockController::with_internal_32kosc(
+            cx.device.GCLK,
+            &mut cx.device.MCLK,
+            &mut cx.device.OSC32KCTRL,
+            &mut cx.device.OSCCTRL,
+            &mut cx.device.NVMCTRL,
+        );
 
-        // Initialize Monotonic
-        Mono::start(cx.device.RTC0);
+        // Start the SysTick
+        Mono::start(cx.core.SYST, 120_000_000);
 
-        // Setup LED
-        let port0 = hal::gpio::p0::Parts::new(cx.device.P0);
-        let led = port0.p0_06.into_push_pull_output(Level::Low).degrade();
+        let pins = Pins::new(cx.device.PORT);
+        let red_led = pins.d13.into();
 
         // Schedule the blinking task
-        blink::spawn().ok();
+        blink::spawn().unwrap();
 
-        (Shared {}, Local { led })
+        (Shared {}, Local { red_led })
     }
 
-    #[task(local = [led])]
+    #[task(local = [red_led])]
     async fn blink(cx: blink::Context) {
-        let blink::LocalResources { led, .. } = cx.local;
-
-        let mut next_tick = Mono::now();
         let mut blink_on = false;
+
         loop {
-            let now = Mono::now();
-            let now_ms: fugit::SecsDurationU64 = now.duration_since_epoch().convert();
-            defmt::println!("Timer {} ({})", now_ms, now.ticks());
+            Mono::delay(500.millis()).await;
 
             blink_on = !blink_on;
             if blink_on {
-                led.set_high().unwrap();
+                cx.local.red_led.set_high().unwrap();
             } else {
-                led.set_low().unwrap();
+                cx.local.red_led.set_low().unwrap();
             }
-
-            next_tick += 1000.millis();
-            Mono::delay_until(next_tick).await;
         }
     }
 }
